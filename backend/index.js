f8995f10236dc90816b924e4d1be77f1cfe88953
etopiei <lj343@icloud.com>
2019-03-17 02:50:41 +1100

    API working now

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./openapi.json');
-const fs = require('fs');
+//const fs = require('fs');
 const fetch = require('node-fetch');
-const path = require('path');
+//const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const Sequelize = require('sequelize');
 
 const app = express();
 const port = 3000;
 
 // the details to access the database will come from environment variables
-const { Client } = require('pg');
-const client = new Client({host: process.env.DB});
-client.connect();
+const sequelize = new Sequelize('postgres', 'postgres', process.env.PGPASSWORD, {
+	host: process.env.DB,
+	dialect: "postgres"
+})
 
+app.use(cors());
 app.use(bodyParser.json());
 
 var options = {
@@ -22,6 +26,7 @@ var options = {
 };
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
+/*
 function get_station_metadata(station_id) {
 	// here fetch metadata from station file
 	filePath = path.join(__dirname, `/../data/output/stop-data-${station_id}.json`);
@@ -43,20 +48,52 @@ function get_station_metadata(station_id) {
 		return {};
 	}
 }
+*/
+
+function get_station_metadata(station_id) {
+	const p = [];
+	p.push(sequelize.query('SELECT * FROM stop_facilities WHERE stopid = ? LIMIT 1', {replacements: [station_id]}));
+	return Promise.all(p).then(result => {
+		const row_data = result[0][0][0];
+		try {
+			return {
+				"cctv": row_data["cctv"],
+				"toilet": row_data["toilet"],
+				"pay_phone": row_data["pay_phone"],
+				"indoor_waiting_area": row_data["indoor_waiting_area"],
+				"sheltered_waiting_area": row_data["sheltered_waiting_area"],
+				"taxi_rank": row_data["taxi_rank"],
+				"lighting": row_data["lighting"],
+				"phone": row_data["phone"],
+				"waiting_room": row_data["waiting_room"],
+				"psos": row_data["psos"],
+				"premium_stop": row_data["premium_stop"],
+				"travellers_aid": row_data["travellers_aid"],
+				"kiosk": row_data["kiosk"]
+			};
+		} catch (e) {
+			console.log("Failed to add data");
+			return null;
+		}
+	}).catch(err => {
+		// stopid probably doesn't exist
+		console.log(err);
+		return null;
+	});
+}
 
 function get_safety_index(stop_id) {
-	const meta_data = get_station_metadata(stop_id);
-	return client.query('SELECT safety_index($1::integer)', [stop_id]).then(safety_index => {
+	const p = [];
+	p.push(get_station_metadata(stop_id));
+	p.push(sequelize.query('SELECT safety_index(?)', {replacements: [stop_id]}));
+	return Promise.all(p).then(resolved => {
 		return {
-			"s_index": safety_index["rows"][0]["safety_index"],
-			"meta": meta_data
+			"s_index": resolved[1][0][0],
+			"meta": resolved[0]
 		};
-	}).catch(db_err => {
-		console.log(db_err);
-		return {
-			"s_index": null,
-			"meta": meta_data
-		};
+	}).catch(err => {
+		console.log(err);
+		return null;
 	});
 }
 
@@ -69,7 +106,6 @@ function get_walking_index(fromLat, fromLon, toLat, toLon) {
 
 app.get('/safety', (req, res) => {
 	const stop = req.query.stopid;
-	console.log(stop);
 	get_safety_index(stop).then(safety_data => {
 		res.json({"safety": safety_data});
 	}).catch(err => {
@@ -77,7 +113,7 @@ app.get('/safety', (req, res) => {
 	});
 });
 
-app.get('/route', async (req, res) => {
+app.post('/route', async (req, res) => {
 	const fromLat = req.body.start.lat;
 	const fromLon = req.body.start.lon;
 	const toLat = req.body.destination.lat;
@@ -92,14 +128,18 @@ app.get('/route', async (req, res) => {
 		routes.forEach(route => {
 			const legs = route["legs"];
 			legs.forEach(leg => {
-				if (leg["MODE"] != "WALK") {
-					promises_waiting.push(get_safety_index(leg["from"]["stopId"].split(":")[1]));
+				if (leg["MODE"] != 'WALK') {
+					try {
+						promises_waiting.push(get_safety_index(leg["from"]["stopId"].split(":")[1]));
+					} catch (_) {
+						promises_waiting.push(get_walking_index(leg["from"]["lat"], leg["from"]["lon"], leg["to"]["lat"], leg["to"]["lon"]));
+					}
 				} else {
 					promises_waiting.push(get_walking_index(leg["from"]["lat"], leg["from"]["lon"], leg["to"]["lat"], leg["to"]["lon"]));
 				}
 			});
 		});
-		Promises.all(promises_waiting).then(resolved => {
+		Promise.all(promises_waiting).then(resolved => {
 			res.json({"routes": routes, "safety": resolved});
 		}).catch(err => {
 			// If this happens, make sure the route is still sent
@@ -107,7 +147,8 @@ app.get('/route', async (req, res) => {
 			res.json({"routes": routes, "safety": null});
 		});
 	}).catch(err => {
-		res.status(500).send(`Failed to get route data: ${JSON.stringify(err)}`);
+		console.log(err);
+		res.status(500).send(`Failed to get route data, or no route options: ${JSON.stringify(err)}`);
 	});
 });
 
