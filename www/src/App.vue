018df167ee71d736ba1ea2c5af0d51919518c27b
Nazaire <dylan.kay@me.com>
2019-03-22 08:39:53 +1100

    Added welcome page

---
+++ www/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app>
+    <!-- <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar> -->
+
+    <v-content>
+      <router-view />
+    </v-content>
+    <v-footer class="footer" text-xs-center>
+        <v-layout justify-center>
+          <v-flex xs12>Please get in touch at<span class="link">dylan.kay@me.com</span></v-flex>
+        </v-layout>
+    </v-footer>
+  </v-app>
+</template>
+
+<style>
+.footer {
+  padding: 0px 20%;
+  text-align:center;
+}
+.link {
+  text-decoration: underline;
+  margin: 0px 5px;
+}
+</style>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ www/src/views/Home.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-container fluid class="home" text-xs-center grid-list-xl>   
+
+    <v-layout row justify-center>
+      <v-flex xs6>
+        <h1 class="brand display-3">lumino.</h1>
+        <span class="subheading">Coming to Melbourne very soon!</span>
+        <v-form>
+          <v-text-field
+            box
+            label="Add yourself to our mailing list"
+            required
+            append-icon="send"
+            @click:append=""
+          ></v-text-field>
+        </v-form>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap justify-center>
+
+      <v-flex xs8>
+
+        <v-sheet
+          class="d-flex sheet"
+          color="grey lighten-3"
+          elevation="0">
+          <v-layout wrap>
+            <v-flex xs12>
+              <h6 class="title">Do you feel unsafe on public transport?</h6>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.</p>
+            </v-flex>
+          </v-layout>
+        </v-sheet>
+
+      </v-flex>
+
+      <v-flex xs8>
+
+        <v-sheet
+          class="d-flex sheet"
+          color="grey lighten-3"
+          elevation="0">
+          <v-layout wrap>
+            <v-flex xs12>
+              <h6 class="title">How does it work?</h6>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.</p>
+            </v-flex>
+          </v-layout>
+        </v-sheet>
+
+      </v-flex>
+
+      <v-flex xs8>
+
+        <v-sheet
+          class="d-flex sheet"
+          color="blue lighten-3"
+          elevation="6">
+          <v-layout wrap>
+            <v-flex xs6>
+              <h6 class="title">Open Source</h6>
+              <p>Check us out on GitHub!</p>
+              <a href="https://github.com/etopiei/lumino">
+                <v-img
+                  :src="require('../assets/github.png')"
+                  contain
+                  class="d-inline-flex"
+                  height="80px"
+                  width="80px"
+                  ></v-img>
+              </a>
+            </v-flex>
+            <v-flex xs6>
+              <h6 class="title">UNIHACK</h6>
+              <p>We built the first version of lumino in 24 hours at Unihack Melbourne. Feel free to try out the UNIHACK version below. <small>Warning! Plenty of bugs!</small></p>
+              <v-btn color="white" block>Try Demo</v-btn>
+              <a class="devpost" href="https://devpost.com/software/lumino">
+                View Lumino on Devpost
+              </a>
+            </v-flex>
+          </v-layout>
+        </v-sheet>
+
+      </v-flex>
+
+    </v-layout>
+
+  </v-container>
+</template>
+
+<style lang="scss" scoped>
+a {
+  // text-decoration: none;
+}
+.brand {
+  font-weight:100;
+}
+.sheet {
+  padding: 20px;
+  h6 {
+    padding-bottom:4px;
+  }
+}
+small {
+  color:blue;
+}
+.devpost {
+  color:white;
+}
+</style>
+
+<script>
+
+  export default {
+    name: 'home',
+    components: {
+      
+    }
+  }
+</script>
